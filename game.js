cb022792da656ae4388aa663a7542c0f80025dd6
水野 <113093738+mi-zu-nox@users.noreply.github.com>
2024-07-07 15:23:35 +0900

    Update game.js

--- game.js
+++ game.js
@@ -25,8 +25,8 @@ const nikuInterval = 2000; // 2秒ごとに生成
 const init = async () => {
     const background = await loadImage(`${IMAGE_PATH}/images/background2.png`);
     const playerImages = [
-        await loadImage(`${IMAGE_PATH}/images/dog2.png`),
-        await loadImage(`${IMAGE_PATH}/images/dog1.png`)
+        await loadImage(`${IMAGE_PATH}/images/dog1.png`),
+        await loadImage(`${IMAGE_PATH}/images/dog2.png`)
     ];
     const leftButtonImg = await loadImage(`${IMAGE_PATH}/images/left1.png`);
     const rightButtonImg = await loadImage(`${IMAGE_PATH}/images/right1.png`);
@@ -126,7 +126,7 @@ const init = async () => {
         // heart.png の表示
         if (heartTimer > 0) {
             ctx.drawImage(heartImg, playerX + playerImages[playerIndex].width - heartImg.width + 10, playerY, heartImg.width / 10, heartImg.height / 10);
-            heartTimer = 2;
+            heartTimer --;
         }
 
         // プレイヤーの描画
